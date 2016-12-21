1364835c2e0e53d12a473e71157b0bda554967a6
Jose Juarez <jose@juarez.me>
2016-12-21 15:44:19 -0800

    added a random number to code and displaying via template

--- script1.py
+++ script1.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
+def the_number():
+    target = randint(1,100)
+    return str(target)
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    num = the_number()
+    return render_template("home.html", num = num)
 
 @app.route('/about/')
 def about():
