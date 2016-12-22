bcbbf7a505a4fb2208fae6158520c3dd49728c69
Jose Juarez <jose@juarez.me>
2016-12-21 18:58:07 -0800

    added data and time on page

--- script1.py
+++ script1.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from random import randint
+import datetime
 
 app = Flask(__name__)
 
@@ -7,10 +8,15 @@ def the_number():
     target = randint(1,100)
     return str(target)
 
+def get_date():
+    now = datetime.datetime.now()
+    return str("{} {}, {} - {}:{}::{}".format(now.strftime("%B"),now.day,now.year, now.time().hour, now.time().minute, now.time().second))
+
 @app.route('/')
 def home():
     num = the_number()
-    return render_template("home.html", num = num)
+    date = get_date()
+    return render_template("home.html", num = num, date = date)
 
 @app.route('/about/')
 def about():
